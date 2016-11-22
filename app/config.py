6c2c0bcba0851dc1bddd2be5a6c50cfc1c97207e
Daisuke Maki <lestrrat+github@gmail.com>
2016-11-22 16:56:52 +0900

    allow setting values from the environment

--- app/config.py
+++ app/config.py
@@ -1,14 +1,31 @@
 import json
+import os
 
 class Config(object):
+    def populate_from_env(self, envname, section, key):
+        v = os.getenv(envname)
+        if v:
+            c = self.section(section, create=True)
+            c[key] = v
+
     def __init__(self, file):
         with open(file, 'r') as f:
             self.cfg = json.load(f)
 
-    def section(self, name):
+        self.populate_from_env('OCTAV_CLIENT_DEBUG', 'OCTAV', 'debug')
+        self.populate_from_env('OCTAV_CLIENT_KEY', 'OCTAV', 'key')
+        self.populate_from_env('OCTAV_CLIENT_SECRET', 'OCTAV', 'secret')
+        for t in ['GITHUB', 'FACEBOOK', 'TWITTER']:
+            self.populate_from_env('ADMINWEB_%s_CLIENT_ID' % t, t, 'client_id')
+            self.populate_from_env('ADMINWEB_%s_SECRET' % t, t, 'client_secret')
+ 
+    def section(self, name, create=False):
         o = self.cfg.get(name)
         if o is None:
-            raise Exception("config section %s is missing" % (name))
+            if create:
+                self.cfg[name] = {}
+            else:
+                raise Exception("config section %s is missing" % (name))
         return o
 
     def googlemap_api_key(self):
